a9c02028f0ad29e99b7e568b0090356f11a01258
iermiloff <109422436+iermiloff@users.noreply.github.com>
2026-06-29 18:47:02 +0300

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,11 @@
+STATUS_LIMITS = {
+    "verified": 1,
+    "trader": 5,
+    "super_trader": 10
+}
+
+STATUS_NAMES = {
+    "verified": "🟢 Верифицированный",
+    "trader": "🔥 Трейдер",
+    "super_trader": "⚡ Супер-трейдер"
+}
